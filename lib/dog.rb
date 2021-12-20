f60b94282cdb828b203faa156d070d8ada43223a
Robert Gaspar <gaspar.robert7@gmail.com>
2021-12-19 21:50:29 -0600

    commit tree 21f2c6a1433baf722ac0264d093c3ec49000af6d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
-class Dog
+require 'pry'
 
+class Dog
+    attr_accessor :name 
+    attr_accessor :breed
+
+    def names(name)
+        @name = name
+    end
+
+    def breeds(breed)
+        @breed = breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Person
+    attr_accessor :name
+    attr_accessor :job
+    
+    def names(name)
+        @name = name
+    end
+
+    def jobs(job)
+        @job = job
+    end
 
 end
